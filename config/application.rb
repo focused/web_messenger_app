028a3754b7c1863015c1b36288baec259c6a127a
Dmitry Novikov <glowinglens@gmail.com>
2014-08-03 21:46:28 +0400

    Setup application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,10 +21,22 @@ module WebMessengerApp
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Moscow'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ru
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      # g.serializer false
+      g.view_specs false
+      g.helper_specs false
+      g.controller_specs false
+      g.model_specs false
+      g.routing_specs false
+      g.request_specs false
+    end
   end
 end
