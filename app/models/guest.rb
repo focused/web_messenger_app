43c3795e917ff70afd6c23db3318524012af1139
Dmitry Novikov <glowinglens@gmail.com>
2014-08-05 01:08:45 +0400

    Add "before_validation" to model Guest

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,9 +1,17 @@
 # TODO: change storage (maybe Redis?)
 
 class Guest < ActiveRecord::Base
+  before_validation do
+    self.name.strip!
+  end
+
   scope :except_one, -> person do
     all.where.not(id: person.id)
   end
 
-  validates :name, presence: true, length: 2..255, uniqueness: true
+  validates :name, {
+    presence: true,
+    length: 2..255,
+    uniqueness: true
+  }
 end
