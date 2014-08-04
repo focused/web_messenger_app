18a951e467cd453b8f60620db288bfcb2e82206a
Dmitry Novikov <glowinglens@gmail.com>
2014-08-04 04:43:53 +0400

    Make specs for Chat model

--- spec/models/messenger/chat_spec.rb
+++ spec/models/messenger/chat_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Messenger::Chat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.actual_for' do
+    it "existing data with 'deleted_at = nil'" do
+      chat = create(:messenger_chat)
+      chats_person = chat.chats_people.actual.first
+
+      expect(Messenger::Chat.actual_for(chats_person.person_id).size).to eq 1
+    end
+
+    it "no data with 'deleted_at = nil'" do
+      chat = create(:messenger_chat)
+      chats_person = chat.chats_people.deleted.first
+
+      expect(Messenger::Chat.actual_for(chats_person.person_id).size).to eq 0
+    end
+  end
 end
